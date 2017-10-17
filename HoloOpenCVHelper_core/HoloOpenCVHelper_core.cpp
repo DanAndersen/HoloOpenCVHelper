56e2b79710060db062810370e84087ffa821d4c4
Andersen <andersed@purdue.lcl>
2017-10-17 16:40:31 -0400

    add functions to get rvec/tvec from solvepnp

--- HoloOpenCVHelper_core/HoloOpenCVHelper_core.cpp
+++ HoloOpenCVHelper_core/HoloOpenCVHelper_core.cpp
@@ -20,6 +20,7 @@ extern "C" {
 			rows = 0;
 			cols = 0;
 			image = NULL;
+			got_valid_pose = false;
 		}
 
 		void setImage(uchar* imageData) {
@@ -37,6 +38,64 @@ extern "C" {
 			// do nothing
 		}
 
+		bool findExtrinsics(
+			int chessX, int chessY, float chessSquareMeters,
+			double cam_mtx_fx, double cam_mtx_fy, double cam_mtx_cx, double cam_mtx_cy,
+			double dist_k1, double dist_k2, double dist_p1, double dist_p2, double dist_k3) {
+
+			bool success = false;
+
+			patternSize.width = chessX;
+			patternSize.height = chessY;
+
+			pointBuf.clear();
+			bool found = cv::findChessboardCorners(gray, patternSize, pointBuf, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FAST_CHECK | CV_CALIB_CB_NORMALIZE_IMAGE);
+
+			if (found) {
+				cv::cornerSubPix(gray, pointBuf, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+
+				cv::Mat cameraMatrix(cv::Size(3, 3), CV_64F, 0.0);
+				cameraMatrix.at<double>(0, 0) = cam_mtx_fx;
+				cameraMatrix.at<double>(1, 1) = cam_mtx_fy;
+				cameraMatrix.at<double>(0, 2) = cam_mtx_cx;
+				cameraMatrix.at<double>(1, 2) = cam_mtx_cy;
+				cameraMatrix.at<double>(2, 2) = 1.0;
+
+				cv::Mat distCoeffs(cv::Size(1, 5), CV_64F, 0.0);
+				distCoeffs.at<double>(0) = dist_k1;
+				distCoeffs.at<double>(1) = dist_k2;
+				distCoeffs.at<double>(2) = dist_p1;
+				distCoeffs.at<double>(3) = dist_p2;
+				distCoeffs.at<double>(4) = dist_k3;
+
+				std::vector<cv::Point3f> obj;	// object points (valid for the latest chess dimensions)
+				obj.clear();
+				for (int i = 0; i < chessY; i++) {
+					for (int j = 0; j < chessX; j++) {
+						obj.push_back(cv::Point3f((float)j * chessSquareMeters, (float)i * chessSquareMeters, 0));
+					}
+				}
+
+				cv::Mat rvec, tvec;
+				success = cv::solvePnP(obj, pointBuf, cameraMatrix, distCoeffs, rvec, tvec);
+
+				if (success) {
+					got_valid_pose = true;
+					rvec_0 = rvec.at<double>(0);
+					rvec_1 = rvec.at<double>(1);
+					rvec_2 = rvec.at<double>(2);
+					tvec_0 = tvec.at<double>(0);
+					tvec_1 = tvec.at<double>(1);
+					tvec_2 = tvec.at<double>(2);
+				}
+				else {
+					got_valid_pose = false;
+				}
+			}
+
+			return success;
+		}
+
 		uchar* getProcessedImage() {
 			if (image == NULL) {
 				std::cerr << "Error : image not set" << std::endl;
@@ -44,7 +103,16 @@ extern "C" {
 			}
 
 			processedImage = cv::Mat(*image);
-			cv::cvtColor(gray, processedImage, CV_GRAY2BGRA);
+
+			if (got_valid_pose) {
+				image->copyTo(processedImage);
+				cv::drawChessboardCorners(processedImage, patternSize, pointBuf, true);
+			}
+			else {
+				cv::cvtColor(gray, processedImage, CV_GRAY2BGRA);
+			}
+
+			//
 			
 			//cv::bitwise_not(*image, processedImage);
 
@@ -64,11 +132,44 @@ extern "C" {
 			return rows;
 		}
 
+		double getRvec0() {
+			return rvec_0;
+		}
+
+		double getRvec1() {
+			return rvec_1;
+		}
+
+		double getRvec2() {
+			return rvec_2;
+		}
+
+		double getTvec0() {
+			return tvec_0;
+		}
+
+		double getTvec1() {
+			return tvec_1;
+		}
+
+		double getTvec2() {
+			return tvec_2;
+		}
+
 	private:
 		cv::Mat * image;
 		cv::Mat gray;
 		int rows, cols;
 		cv::Mat processedImage;
+		bool got_valid_pose;
+		double rvec_0;
+		double rvec_1;
+		double rvec_2;
+		double tvec_0;
+		double tvec_1;
+		double tvec_2;
+		std::vector<cv::Point2f> pointBuf;
+		cv::Size patternSize;
 	};
 
 	chessPoseController* ac;
@@ -108,6 +209,74 @@ extern "C" {
 		if (initialized)
 			ac->detect();
 	}
+
+	__declspec(dllexport) double GetRvec0() {
+		if (initialized) {
+			return ac->getRvec0();
+		}
+		else {
+			return -2;
+		}
+	}
+
+	__declspec(dllexport) double GetRvec1() {
+		if (initialized) {
+			return ac->getRvec1();
+		}
+		else {
+			return -2;
+		}
+	}
+
+	__declspec(dllexport) double GetRvec2() {
+		if (initialized) {
+			return ac->getRvec2();
+		}
+		else {
+			return -2;
+		}
+	}
+
+	__declspec(dllexport) double GetTvec0() {
+		if (initialized) {
+			return ac->getTvec0();
+		}
+		else {
+			return -2;
+		}
+	}
+
+	__declspec(dllexport) double GetTvec1() {
+		if (initialized) {
+			return ac->getTvec1();
+		}
+		else {
+			return -2;
+		}
+	}
+
+	__declspec(dllexport) double GetTvec2() {
+		if (initialized) {
+			return ac->getTvec2();
+		}
+		else {
+			return -2;
+		}
+	}
+
+	__declspec(dllexport) bool findExtrinsics(
+		int chessX, int chessY, float chessSquareMeters,
+		double cam_mtx_fx, double cam_mtx_fy, double cam_mtx_cx, double cam_mtx_cy,
+		double dist_k1, double dist_k2, double dist_p1, double dist_p2, double dist_k3) {
+		if (initialized) {
+			return ac->findExtrinsics(chessX, chessY, chessSquareMeters, cam_mtx_fx, cam_mtx_fy, cam_mtx_cx, cam_mtx_cy, dist_k1, dist_k2, dist_p1, dist_p2, dist_k3);
+		}
+		else {
+			return false;
+		}
+	}
+
+	
 
 	__declspec(dllexport) int getRows() {
 		if (initialized)
